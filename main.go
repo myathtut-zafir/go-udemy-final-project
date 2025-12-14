f298c07bfa9aba57933652ab5a5021a6d1b8f032
myathtut-zafir <aa.htut@gmail.com>
2025-12-14 12:11:36 +0700

    feat: get event by id

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"example.com/event-api/db"
 	"example.com/event-api/models"
@@ -12,6 +13,7 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEventByID)
 	server.POST("/events", createEvent)
 	server.Run() // listen and seserverve on 0.0.0.0:8080
 }
@@ -27,7 +29,7 @@ func getEvents(context *gin.Context) {
 }
 func createEvent(context *gin.Context) {
 	var event models.Event
-	err := context.ShouldBindJSON(& )
+	err := context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -40,4 +42,17 @@ func createEvent(context *gin.Context) {
 		return
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "Created", "event": event})
+}
+func getEventByID(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+	event, err := models.GetEventByID(id)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, event)
 }
