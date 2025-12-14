69bcb670a3da8819c57163af8d01dbce054cb1f6
myathtut-zafir <aa.htut@gmail.com>
2025-12-14 12:00:01 +0700

    feat: add data to database

--- main.go
+++ main.go
@@ -17,18 +17,27 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
 	context.JSON(http.StatusOK, events)
 }
 func createEvent(context *gin.Context) {
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(& )
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	event.ID = 1
-	event.UserID = 1
-	event.Save()
+	// event.ID = 1
+	// event.UserID = 1
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 	context.JSON(http.StatusCreated, gin.H{"message": "Created", "event": event})
 }
--- models/event.go
+++ models/event.go
@@ -1,23 +1,56 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/event-api/db"
+)
 
 type Event struct {
-	ID          int
-	Name        string `binding:"required"`
-	Description string `binding:"required"`
-	Location    string 	`binding:"required"`
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
 	DateTime    time.Time `json:"date_time" binding:"required"`
 	UserID      int
 }
 
 var events = []Event{}
 
-func (e Event) Save() {
-	events = append(events, e)
+func (e Event) Save() error {
+	query := "INSERT INTO events (name, description, location, date_time, user_id) VALUES (?, ?, ?, ?, ?)"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
+	// events = append(events, e)
 	// add DB save logic here
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+	for rows.Next() {
+		var e Event
+		err := rows.Scan(&e.ID, &e.Name, &e.Description, &e.Location, &e.DateTime, &e.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, e)
+	}
+	return events, nil
 }
