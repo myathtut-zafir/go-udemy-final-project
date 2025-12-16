b72ee740661bdb91601e6b7e35aace5a1f1adc26
myathtut-zafir <aa.htut@gmail.com>
2025-12-16 22:13:30 +0700

    feat: add route group and add middleware

--- routes/events.go
+++ routes/events.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"example.com/event-api/models"
-	"example.com/event-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,22 +18,14 @@ func getEvents(context *gin.Context) {
 	context.JSON(http.StatusOK, events)
 }
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authrized"})
-		return
-	}
-	userId, err := utils.VerifyToken(token)
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
+
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	userId := context.GetInt64("userId")
 	event.UserID = userId
 	err = event.Save()
 	if err != nil {
--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,19 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"example.com/event-api/middlewares"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEventByID)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+	auth := server.Group("/")
+	auth.Use(middlewares.Authenticate)
+	auth.POST("/events", createEvent)
+	auth.PUT("/events/:id", updateEvent)
+	auth.DELETE("/events/:id", deleteEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
