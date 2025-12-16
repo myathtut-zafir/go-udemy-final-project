15d38cf7b47c228ccf61cd40f6baf6a4146a67fd
myathtut-zafir <aa.htut@gmail.com>
2025-12-16 22:23:06 +0700

    feat: create register table

--- db/db.go
+++ db/db.go
@@ -44,5 +44,18 @@ func createTables() {
 	if err != nil {
 		print("Error creating table")
 	}
+	createRegistrationsTable := `
+	CREATE TABLE IF NOT EXISTS registrations (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		event_id INTEGER NOT NULL,
+		user_id INTEGER NOT NULL,
+		FOREIGN KEY (event_id) REFERENCES events(id),
+		FOREIGN KEY (user_id) REFERENCES users(id),
+		UNIQUE(user_id, event_id)
+	);`
+	_, err = DB.Exec(createRegistrationsTable)
+	if err != nil {
+		print("Error creating table")
+	}
 
 }
